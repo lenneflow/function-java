d316063ee0b0d1022efd6b2fddc19c813217f579
Idrissa GANEMTORE <dev@ganemtore.net>
2024-08-10 09:05:22 -0400

    First commit

---
+++ src/main/java/de/lenneflow/functionjava/FunctionJavaApplication.java
@@ -0,0 +1,13 @@
+package de.lenneflow.functionjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/lenneflow/functionjava/FunctionJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lenneflow.functionjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
